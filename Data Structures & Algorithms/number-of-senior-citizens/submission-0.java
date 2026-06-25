69c9face0d01c0ffcbd120403b9c9ab52655d75a
Nikolas Posavec <155776636+NikolasPosavec@users.noreply.github.com>
2026-06-25 18:29:52 -0400

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/number-of-senior-citizens/submission-0.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int counter = 0;
+
+        for(String s : details) {
+            if(s.charAt(11) >= '6') {
+                counter++;
+            }
+        }
+
+        return counter;
+    }
+}
