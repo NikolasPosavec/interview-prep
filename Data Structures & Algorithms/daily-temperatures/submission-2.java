066f3cd2a5613617a31ddcb6ca77521d2912e204
Nikolas Posavec <155776636+NikolasPosavec@users.noreply.github.com>
2026-08-12 12:33:36 -0400

    Add: daily-temperatures - submission-2

---
+++ Data Structures & Algorithms/daily-temperatures/submission-2.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
+        int[] result = new int[temperatures.length];
+        
+        for(int t : temperatures) {
+            stack.push(t);
+        }
+        
+        for(int i = 0; i < temperatures.length; i++) {
+            int count = 0;
+            Stack<Integer> copy = new Stack<>();
+            copy.addAll(stack);
+            for(int j = i; j > 0; j--) {
+                copy.pop();
+            }
+            while(copy.peek() < temperatures[i]) {
+                count++;
+                copy.pop();
+            }
+            result[i] = count;
+        }
+
+        return result;
+    }
+}
