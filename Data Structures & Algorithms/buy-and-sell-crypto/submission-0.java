f23b7aa49430d86fcaad9efc666b3dac58d2e9a8
Nikolas Posavec <155776636+NikolasPosavec@users.noreply.github.com>
2026-08-17 11:03:17 -0400

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices.length < 2) {
+            return 0;
+        }
+        
+        int l = 0;
+        int r = 1;
+        int profit = 0;
+
+        while(r < prices.length) {
+            if(prices[r] < prices[l]) {
+                l++;
+                r++;
+            } else if(prices[r] > prices[l]) {
+                profit = (prices[r] - prices[l] > profit) ? prices[r] - prices[l] : profit;
+                r++;
+            } else {
+                r++;
+            }
+        }
+
+        return profit;
+    }
+}
