a563f2a471ee99b5a26adfff6aa2bb81021d1d4f
Nikolas Posavec <155776636+NikolasPosavec@users.noreply.github.com>
2026-08-18 17:07:49 -0400

    Add: eating-bananas - submission-5

---
+++ Data Structures & Algorithms/eating-bananas/submission-5.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int k = 0;
+        int max = -1;
+        for(int i : piles) {
+            max = (max < i) ? i : max;
+        }
+        int l = 1;
+        int r = max;
+        
+        while(l <= r) {
+            int mid = (l + r) / 2;
+            int hours = h;
+            for(int p : piles) {
+                hours -= Math.ceil((double)(p) / mid);
+            }
+                
+            if(hours < 0) {
+                l = mid + 1;
+            } else {
+                k = mid;
+                r = mid - 1;
+            }
+        }
+        
+        return k;
+    }
+}
