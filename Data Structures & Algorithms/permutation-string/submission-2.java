e013bd2cab139f90caca968e3419f7031f7ffb1e
Nikolas Posavec <155776636+NikolasPosavec@users.noreply.github.com>
2026-08-20 13:12:08 -0400

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int p1 = 0;
+        int p2 = s1.length() - 1;
+        List<String> seen = new ArrayList<>();
+
+        while(p2 < s2.length()) {
+            seen = new ArrayList<>();
+            for(char ch : s2.substring(p1, p2 + 1).toCharArray()) {
+                seen.add("" + ch);
+            }
+
+            boolean flag = false;
+            for(int i = 0; i < s1.length(); i++) {
+                seen.remove("" + s1.charAt(i));
+            }
+
+            if(seen.size() == 0) {
+                return true;
+            }
+
+            p1++;
+            p2++;
+        }
+
+        return false;
+    }
+}
