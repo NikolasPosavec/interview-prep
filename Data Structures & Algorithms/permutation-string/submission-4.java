4e8200cd60dc635b09a275b019ea70e6f9fdff45
Nikolas Posavec <155776636+NikolasPosavec@users.noreply.github.com>
2026-08-20 13:20:52 -0400

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.java
@@ -0,0 +1,37 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int p1 = 0;
+        int p2 = s1.length() - 1;
+        Map<Character, Integer> s1Mapped = new HashMap<>();
+        Map<Character, Integer> copy = new HashMap<>();
+        for(char ch : s1.toCharArray()) {
+            if(s1Mapped.containsKey(ch)) {
+                s1Mapped.put(ch, s1Mapped.get(ch) + 1);
+            } else {
+                s1Mapped.put(ch, 1);
+            }
+        }
+
+        while(p2 < s2.length()) {
+            copy = new HashMap<>(s1Mapped);
+            boolean flag = false;
+            for(char ch : s2.substring(p1, p2 + 1).toCharArray()) {
+                if(copy.containsKey(ch) && copy.get(ch) > 0) {
+                    copy.put(ch, copy.get(ch) - 1);
+                } else {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if(!flag) {
+                return true;
+            }
+
+            p1++;
+            p2++;
+        }
+
+        return false;
+    }
+}
