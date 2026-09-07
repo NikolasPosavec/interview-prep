ac8f5a9251897f220446ec776f59b27b968a44be
Nikolas Posavec <155776636+NikolasPosavec@users.noreply.github.com>
2026-09-07 16:18:28 -0400

    Add: sliding-window-maximum - submission-4

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-4.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if(k <= 1) {
+            return nums;
+        }
+
+        PriorityQueue<Integer> maxQueue = new PriorityQueue<>(Collections.reverseOrder());
+        int p1 = 0;
+        int p2 = k - 1;
+        int[] result = new int[nums.length - k + 1];
+        int count = 0;
+
+        while(p2 < nums.length) {
+            if(p1 == 0) {
+                for(int i = p1; i <= p2; i++) {
+                    maxQueue.add(nums[i]);
+                }
+            } else {
+                maxQueue.add(nums[p2]);
+            }
+
+            result[count++] = maxQueue.peek();
+            p1++;
+            p2++;
+        }
+
+        return result;
+    }
+}
